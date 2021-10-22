d54ebab6aac3bc86d68230febbc867845afcf87c
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-10-22 15:50:31 +0200

    favorite bug fixed

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import FavoriteSong from "./FavoriteSong";
 
 const Favorite = () => {
@@ -10,6 +10,12 @@ const Favorite = () => {
     <div className="col-12 col-md-9 offset-md-3 mainPage">
       <Row>
         <div className="col-md-12 p-5">
+          <h4 className="text-white mb-5">Favorite songs:</h4>
+          <Row className="text-white">
+            <Col>Title/Artist</Col>
+            <Col>Album</Col>
+            <Col>Duration</Col>
+          </Row>
           <Row>
             <div className="col-md-10 mb-5" id="trackList">
               {favoriteSongs.map((song) => (
--- src/components/FavoriteSong.jsx
+++ src/components/FavoriteSong.jsx
@@ -33,65 +33,57 @@ const FavoriteSong = ({ track, albumImg, albumTitle }) => {
   };
 
   return (
-    <>
-      <h4 className="text-white mb-5">Favorite songs:</h4>
-      <Row className="text-white">
-        <Col className="d-flex justify-content-center">Title/Artist</Col>
-        <Col className="d-flex justify-content-center">Album</Col>
-        <Col className="d-flex justify-content-center">Duration</Col>
-      </Row>
-      <div className="py-3 trackHover d-flex justify-content-between">
-        <Col>
-          <span
-            className="card-title trackHover px-3 "
-            style={{ color: "white" }}
-          >
-            <div className="d-flex">
-              <div className="d-flex align-items-center">
-                {isItPlayerSong ? (
-                  <Pause className="mr-3" />
-                ) : (
-                  <Play
-                    className="mr-3"
-                    onClick={() =>
-                      dispatch(addToPlayer({ ...track, cover: albumImg }))
-                    }
-                  />
-                )}
-              </div>
-              <img
-                src={track.cover}
-                style={{ width: "55px", height: "55px", marginRight: "10px" }}
-              />
-              <div>
-                <div>{track.title}</div>
-                <div className="text-muted">{track.artist.name}</div>
-              </div>
+    <div className="py-3 trackHover d-flex justify-content-between">
+      <Col>
+        <span
+          className="card-title trackHover px-3 "
+          style={{ color: "white" }}
+        >
+          <div className="d-flex">
+            <div className="d-flex align-items-center">
+              {isItPlayerSong ? (
+                <Pause className="mr-3" />
+              ) : (
+                <Play
+                  className="mr-3"
+                  onClick={() =>
+                    dispatch(addToPlayer({ ...track, cover: albumImg }))
+                  }
+                />
+              )}
             </div>
-          </span>
-        </Col>
-        <Col className="d-flex align-items-center">
-          <div className="text-white ">{track.albumTitle}</div>
-        </Col>
-        <Col className="d-flex justify-content-end align-items-center">
-          <small className="duration" style={{ color: "white" }}>
-            {Math.floor(parseInt(track.duration) / 60)}:
-            {parseInt(track.duration) % 60 < 10
-              ? "0" + (parseInt(track.duration) % 60)
-              : parseInt(track.duration) % 60}
-            {isLiked ? (
-              <HeartFill
-                style={{ color: "green" }}
-                className="ml-3"
-                onClick={() => toggle()}
-              />
-            ) : (
-              <Heart className="ml-3" onClick={() => toggle()} />
-            )}
-          </small>
-        </Col>
-      </div>
-    </>
+            <img
+              src={track.cover}
+              style={{ width: "55px", height: "55px", marginRight: "10px" }}
+            />
+            <div>
+              <div>{track.title}</div>
+              <div className="text-muted">{track.artist.name}</div>
+            </div>
+          </div>
+        </span>
+      </Col>
+      <Col className="d-flex align-items-center">
+        <div className="text-white ">{track.albumTitle}</div>
+      </Col>
+      <Col className="d-flex justify-content-end align-items-center">
+        <small className="duration" style={{ color: "white" }}>
+          {Math.floor(parseInt(track.duration) / 60)}:
+          {parseInt(track.duration) % 60 < 10
+            ? "0" + (parseInt(track.duration) % 60)
+            : parseInt(track.duration) % 60}
+          {isLiked ? (
+            <HeartFill
+              style={{ color: "green" }}
+              className="ml-3"
+              onClick={() => toggle()}
+            />
+          ) : (
+            <Heart className="ml-3" onClick={() => toggle()} />
+          )}
+        </small>
+      </Col>
+    </div>
   );
 };
 
