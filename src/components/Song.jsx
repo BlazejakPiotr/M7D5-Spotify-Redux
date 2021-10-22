343aedf632a42400a5f0065ed6f1713698fb1635
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-10-22 11:39:20 +0200

    like toggle done

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,23 +2,26 @@ import React, { useEffect } from "react";
 import { Heart } from "react-bootstrap-icons";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { addToFavorite } from "../redux/actions";
+import { addToFavorite, removeFromFavorite } from "../redux/actions";
 
 const Song = ({ track }) => {
   const dispatch = useDispatch();
   const isLiked = useSelector((state) =>
     state.favorite.songs.find((t) => t.id === track.id)
   );
-  useEffect(() => {
-    console.log(isLiked);
-  }, []);
 
-  const toggle = () => {};
+  const toggle = () => {
+    if (isLiked === undefined) {
+      dispatch(addToFavorite(track));
+    } else {
+      dispatch(removeFromFavorite(track));
+    }
+  };
 
   return (
     <div className="py-3 trackHover">
       <span className="card-title trackHover px-3" style={{ color: "white" }}>
-        <Heart onClick={() => dispatch(addToFavorite(track))} />
+        <Heart onClick={() => toggle()} />
         {track.title}
       </span>
 
